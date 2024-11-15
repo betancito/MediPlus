6c1996d0b48c608b4ae7b9a577ea49497480d9dd
betancito <jeronimobeta90@gmail.com>
2024-11-15 14:54:12 -0500

    first commit

---
+++ src/main/java/com/appointments/medical/MediPlusApplication.java
@@ -0,0 +1,13 @@
+package com.appointments.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appointments/medical/MediPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointments.medical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
